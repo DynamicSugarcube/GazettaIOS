11892a1b3d7865db71b463ebd776a9ef7dbae8bd
Vsevolod Sharov <vsevolod.sharov@gmail.com>
2021-02-02 12:36:17 +0300

    FIX: Fix publisher name
    Retrieve a source name like BBC, CNN, etc. instead of an author name as a publisher

--- GazettaIOS/Model/NewsArticle.swift
+++ GazettaIOS/Model/NewsArticle.swift
@@ -7,18 +7,36 @@
 
 import Foundation
 
-struct NewsArticle: Decodable {
+struct NewsArticle {
     let publisher: String?
     let title: String?
     let newsUrl: String?
     let imageUrl: String?
     let when: Date?
+}
 
-    private enum CodingKeys: String, CodingKey {
-        case publisher = "author"
+// MARK: - Decodalbe
+extension NewsArticle: Decodable {
+    private enum ArticleCodingKeys: String, CodingKey {
         case title
         case newsUrl = "url"
         case imageUrl = "urlToImage"
         case when = "publishedAt"
+        case source
+    }
+
+    private enum SourceCodingKeys: String, CodingKey {
+        case publisher = "name"
+    }
+
+    init(from decoder: Decoder) throws {
+        let articleInfo = try decoder.container(keyedBy: ArticleCodingKeys.self)
+        title = try articleInfo.decode(String.self, forKey: .title)
+        newsUrl = try articleInfo.decode(String.self, forKey: .newsUrl)
+        imageUrl = try articleInfo.decode(String.self, forKey: .imageUrl)
+        when = try articleInfo.decode(Date.self, forKey: .when)
+
+        let sourceInfo = try articleInfo.nestedContainer(keyedBy: SourceCodingKeys.self, forKey: .source)
+        publisher = try sourceInfo.decode(String.self, forKey: .publisher)
     }
 }
